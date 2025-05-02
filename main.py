b70cb66f2f142015d10018a4898d6ea389e0caca
vazquezea96 <vazquezeduardo1278@gmail.com>
2025-05-01 23:18:32 -0700

    Completed L3: Read File & L4: Count Words

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-def get_book_text():
-    pass
+def get_book_text(book):
+    with open(book, 'r', encoding='utf-8') as file:
+        text = file.read()
+    return text
+
+def count_words(words):
+    document = get_book_text(words)
+
+    string = document.split()
+
+    count = len(string)
+
+    return f"{count} words found in the document."
 
 def main():
-    pass
+    get_book_text("./books/frankenstein.txt")
+    print(count_words("./books/frankenstein.txt"))
+
 
 main()
