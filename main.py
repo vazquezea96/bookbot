8f75784f5e3b5dc376e29107dfbc6eb19a2124b7
vazquezea96 <vazquezeduardo1278@gmail.com>
2025-07-18 14:32:38 -0700

    Finished Count Characters challenge.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_count
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document.")
+    # print(f"{num_words} words found in the document.")
+    char_count = get_character_count(text)
+    print(f"{char_count}")
+
 
 def get_book_text(path):
     with open(path, 'r', encoding='utf-8') as file:
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_character_count(text):
+    character_count = {}
+    lowered_text = text.lower()
+
+    for char in lowered_text:
+        if char in character_count:
+            character_count[char] += 1
+        else:
+            character_count[char] = 1
+
+    return character_count
